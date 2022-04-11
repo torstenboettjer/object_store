899cc0afa9c700371a9e4c303fa3a2d5a2e04fcc
Torsten Boettjer <57480261+torstenboettjer@users.noreply.github.com>
2022-04-11 19:07:53 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,2 +1,11 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+# Outputs
+output "bucket_name" {
+  value = oci_objectstorage_bucket.tf_bucket.name
+}
+
+output "bucket_id" {
+  value = oci_objectstorage_bucket.tf_bucket.bucket_id
+}
