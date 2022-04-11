ea24f77e3191b2249437f974321766d3168b2219
Torsten Boettjer <57480261+torstenboettjer@users.noreply.github.com>
2022-04-11 19:05:58 +0200

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,2 +1,10 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+# Resources
+resource "oci_objectstorage_bucket" "firstasset" {
+  compartment_id = var.compartment_id
+  name           = var.bucket_name
+  namespace      = var.bucket_namespace
+  access_type    = var.bucket_access_type
+}
